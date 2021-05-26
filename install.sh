30b58c3acf22921672b38760798e9ebc7a525e30
mdatikshahriar <md.atikshahriar728@gmail.com>
2021-05-26 19:55:34 +0600

    minor update

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
-mkdir temp
-cp -r fabric-samples/goods-ledger temp
-cp fabric-samples/run.sh temp
-cp fabric-samples/stop.sh temp
-cp fabric-samples/.gitignore temp
+mkdir .temp
+attrib +h .temp
+cp -r fabric-samples/goods-ledger .temp
+cp fabric-samples/run.sh .temp
+cp fabric-samples/stop.sh .temp
+cp fabric-samples/.gitignore .temp
 rm -rf fabric-samples
 
 curl https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s
 
-cp -r temp/goods-ledger fabric-samples
-cp temp/run.sh fabric-samples
-cp temp/stop.sh fabric-samples
-cp temp/.gitignore fabric-samples
-rm -rf temp
+cp -r .temp/goods-ledger fabric-samples
+cp .temp/run.sh fabric-samples
+cp .temp/stop.sh fabric-samples
+cp .temp/.gitignore fabric-samples
+rm -rf .temp
 
 cd fabric-samples/goods-ledger
 
--- uninstall.sh
+++ uninstall.sh
@@ -4,15 +4,16 @@ cd fabric-samples
 
 cd ..
 
-mkdir temp
-cp -r fabric-samples/goods-ledger temp
-cp fabric-samples/run.sh temp
-cp fabric-samples/stop.sh temp
-cp fabric-samples/.gitignore temp
+mkdir .temp
+attrib +h .temp
+cp -r fabric-samples/goods-ledger .temp
+cp fabric-samples/run.sh .temp
+cp fabric-samples/stop.sh .temp
+cp fabric-samples/.gitignore .temp
 rm -rf fabric-samples/*
 
-cp -r temp/goods-ledger fabric-samples
-cp temp/run.sh fabric-samples
-cp temp/stop.sh fabric-samples
-cp temp/.gitignore fabric-samples
-rm -rf temp
+cp -r .temp/goods-ledger fabric-samples
+cp .temp/run.sh fabric-samples
+cp .temp/stop.sh fabric-samples
+cp .temp/.gitignore fabric-samples
+rm -rf .temp
